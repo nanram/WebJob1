eee8e49c29a3c8a5b31c1805d8ca10d8536dbecd
Nanduri Sri Ranga Ramanujam <nanram@microsoft.com>
2018-10-25 11:47:15 +0530

    TestGitRepository

--- WebJob1/Functions.cs
+++ WebJob1/Functions.cs
@@ -14,8 +14,7 @@ namespace WebJob1
         // on an Azure Queue called queue.
         public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
         {
-            log.WriteLine(message);
-            log.WriteLine(message);
+            log.WriteLine(message);            
         }
     }
 }
